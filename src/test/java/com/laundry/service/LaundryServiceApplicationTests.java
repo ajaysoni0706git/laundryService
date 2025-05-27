a8d1ce4f6ddac5d886d948cab054f0157c84055c
ajaysoni0706git <ajsoni.git@gmail.com>
2025-05-27 14:40:48 +0530

    setting up springboot project for laundry services application

---
+++ src/test/java/com/laundry/service/LaundryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laundry.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
